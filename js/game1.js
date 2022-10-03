2a64f19fdb34f917a93707d4dafd9e6827b018bc
suhaillahmad <suhaillahmadd0@gmail.com>
2022-10-03 22:38:10 +0530

    Added an interactive hammer

--- js/game1.js
+++ js/game1.js
@@ -1,3 +1,17 @@
+const cursor = document.querySelector(".cursor");
+
+window.addEventListener("mousemove", (e) => {
+  cursor.style.top = e.pageY + "px";
+  cursor.style.left = e.pageX + "px";
+});
+
+window.addEventListener("mousedown", () => {
+  cursor.classList.add("active");
+});
+window.addEventListener("mouseup", () => {
+  cursor.classList.remove("active");
+});
+
 const spaceShips = document.querySelectorAll(".space");
 const aliens = document.querySelectorAll(".alien");
 let flag = true;
